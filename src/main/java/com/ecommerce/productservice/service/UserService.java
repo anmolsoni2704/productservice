32407d019370be4d85d1ab34e0cbd9937517f9ca
anmolsoni2704 <anmolsoni2704@gmail.com>
2024-05-02 23:24:59 +0530

    added fetch types

--- src/main/java/com/ecommerce/productservice/service/UserService.java
+++ src/main/java/com/ecommerce/productservice/service/UserService.java
@@ -1,11 +1,14 @@
 package com.ecommerce.productservice.service;
 
+import com.ecommerce.productservice.dtos.GetInstructorDto;
+import com.ecommerce.productservice.models.Batch;
 import com.ecommerce.productservice.models.Instructor;
 import com.ecommerce.productservice.models.User;
 import com.ecommerce.productservice.repository.InstructorRepository;
 import com.ecommerce.productservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -42,7 +45,25 @@ public class UserService {
         return userRepository.findByName(name);
     }
 
-    public List<Instructor> getInstructorByName(String name) {
-        return instructorRepository.findByName(name);
+    public List<GetInstructorDto> getInstructorByName(String name) {
+        List<Instructor> instructors = instructorRepository.findByName(name);
+
+        List<GetInstructorDto> instructorDtos = new ArrayList<>();
+        for (Instructor instructor : instructors) {
+            GetInstructorDto getInstructorDto = new GetInstructorDto();
+            getInstructorDto.setId(instructor.getId());
+            getInstructorDto.setName(instructor.getName());
+            getInstructorDto.setEmail(instructor.getEmail());
+            List<String> batchNames = new ArrayList<>();
+            List<Long> ids = new ArrayList<>();
+            for (Batch batch : instructors.get(0).getBatch()) {
+                batchNames.add(batch.getName());
+                ids.add(batch.getId());
+            }
+            getInstructorDto.setBatchName(batchNames);
+            getInstructorDto.setBatchId(ids);
+            instructorDtos.add(getInstructorDto);
+        }
+        return instructorDtos;
     }
 }
